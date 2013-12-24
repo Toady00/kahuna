a1f9f18a59cfa1b886e6d1eea6f4952e96d99192
Brandon Dennis <toady00@gmail.com>
2013-12-24 14:35:02 -0500

    Remove stupid comments from experiments

--- lib/kahuna/handler.rb
+++ lib/kahuna/handler.rb
@@ -5,24 +5,6 @@ module Kahuna
   class Handler
     attr_reader :event, :members
 
-    # def self.build
-    #   klass = Class.new(BasicObject) do
-    #     def inspect
-    #       "<handler>"
-    #     end
-
-    #     klass = self
-    #     define_method(:class) { klass }
-    #   end
-
-    #   builder = ActionClassBuilder.new(klass)
-    #   yield(builder) if block_given?
-    #   unless builder.interface_defined?
-    #     builder.respond_to_missing_with_nil
-    #   end
-    #   klass
-    # end
-
     def initialize(data, &block)
       @event = Event.new data
       unless event.type == 'user'
@@ -31,19 +13,9 @@ module Kahuna
       @action = block_given? ? block : ->(event, members) {}
     end
 
-    # def to_string
-    #   <<-EOF
-    #     DATA: #@data
-    #     EVENT: #@event
-    #     NAME: #@name
-    #     ROLE: #@role
-    #     USER EVENT: #@user_event
-    #     LAMPORT TIME: #@lamport_time
-    #   EOF
-    # end
-
-    def execute
-      @action.call(event, members)
+    def execute &block
+      block_to_call = block_given? ? block : @action
+      block_to_call.call(event, members)
     end
   end
 end
