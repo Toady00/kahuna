c8b0d7987e4adea6d4ba97ea6d6e5f13acba59f5
Brandon Dennis <toady00@gmail.com>
2013-12-28 19:27:37 -0500

    Refactor Handler initializer
    Pulled logic for whether or not to build a MemberCollection into a private method.
    
    Still needs work

--- lib/kahuna/handler.rb
+++ lib/kahuna/handler.rb
@@ -6,16 +6,23 @@ module Kahuna
     attr_reader :event, :members
 
     def initialize(data, &block)
-      @event = Event.new data
-      unless event.type == 'user'
-        @members = MemberCollection.new data
-      end
-      @action = block_given? ? block : ->(event, members) {}
+      @event   = Event.new data
+      @members = build_member_collection data
+      @action  = block_given? ? block : ->(event, members) {}
     end
 
     def execute &block
       block_to_call = block_given? ? block : @action
       block_to_call.call(event, members)
+    end
+
+  private
+    def build_member_collection(data)
+       if event.type == 'user'
+        []
+      else
+        MemberCollection.new data
+      end
     end
   end
 end
